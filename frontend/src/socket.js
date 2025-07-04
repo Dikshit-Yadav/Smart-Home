ee6306666494b76e3e9f9ff7fc30fd8bb0e8d993
Dikshit Yadav <2004dikshityadav@gmail.com>
2025-07-04 17:18:04 +0530

    UnderProcess

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 // const socket = io('http://localhost:5000',
 
-  const socket = io(import.meta.env.VITE_SOCKET_URL, {
+  const socket = io(import.meta.env.VITE_API_URL, {
   transports: ['websocket'],
   withCredentials: true
 });
